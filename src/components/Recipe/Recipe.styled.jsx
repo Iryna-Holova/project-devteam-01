7f699c80faab6f83500e776fc39c9ab241302fa9
KlassArtem <1trabl123@gmail.com>
2023-07-11 22:24:45 +0300

    fix02

--- src/components/Recipe/Recipe.styled.jsx
+++ src/components/Recipe/Recipe.styled.jsx
@@ -49,17 +49,5 @@ export const Container = styled.div`
   padding-right: 16px;
   min-width: 320px;
 
-  @media screen and (max-width: 767px) {
-    max-width: 767px;
-  }
-
-
-  @media screen and (min-width: 768px) {
-    padding-left: 32px;
-    padding-right: 32px;
-  }
-  @media screen and (min-width: 1440px) {
-    padding-left: 100px;
-    padding-right: 100px;
   }
 `;
