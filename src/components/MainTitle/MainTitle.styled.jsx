09117e2348c8f5874c658cb4c7de7bfefbdec7ab
Khavkin <hdm@ukr.net>
2023-07-11 15:52:56 +0300

    Add device adaptation

--- src/components/MainTitle/MainTitle.styled.jsx
+++ src/components/MainTitle/MainTitle.styled.jsx
@@ -1,12 +1,20 @@
 import styled from 'styled-components';
 
 const Title = styled.h2`
-margin-top: 50px;
-color: #001833;
-font-size: 28px;
-font-weight: 600;
-line-height: 28px;
-letter-spacing: -0.56px;
+  margin-top: 50px;
+  color: #001833;
+  font-size: 28px;
+  font-weight: 600;
+  line-height: 1;
+  letter-spacing: -0.56px;
+  @media screen and (min-width: 768px) {
+    margin-top: 72px;
+    font-size: 32px;
+  }
+  @media screen and (min-width: 1440px) {
+    margin-top: 100px;
+    font-size: 44px;
+  }
 `;
 
 export default Title;
