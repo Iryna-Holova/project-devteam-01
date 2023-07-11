7319abcbdd201d11a1c59c643ec5a26bb5574237
AnastasiaMosevych <ukrainischesmaedel@yahoo.de>
2023-07-11 20:45:25 +0200

    added rendering of the recipe card, added margin for h2

--- src/components/CategoriesPreview/CategoriesList/CategoriesList.jsx
+++ src/components/CategoriesPreview/CategoriesList/CategoriesList.jsx
@@ -4,29 +4,39 @@ import { ListItem } from "./CategoriesList.styled";
 // TODO: uncomment when merged with main, because deleted buttons folder 
 // import { SeeAllButton } from "components/Buttons/Buttons.styled";
 import { getRecipesMainAPI } from "api/recipesMain";
+import { Recipe, RecipeImg, Description, RecipeContainer, RecipeNavLink, List} from "components/RecipeGallery/RecipeGallery.styled";
+import IngredientsPlaceholder from '../../../assets/food-default.svg';
 
 const CategoriesList = () => {
-    const [recipes, setRecipes] = useState(null);
+    const [recipes, setRecipes] = useState([]);
     useEffect(() => {
         getRecipesMainAPI().then(recipesMain => setRecipes(recipesMain))
     }, [])
     return (
-        <ul>
+        <List>
             {recipes && Object.keys(recipes).map(category => (
                 <ListItem key={category}>
                     <Title>
                         {category}
                     </Title>
-                    {recipes[category].map((recipe => (
-                        <div key={recipe._id}>
-                            {recipe.title}
-                           <img alt="" src={recipe.preview}/> 
-                        </div>
-                    )))}
+                    <List>
+                        {recipes[category].map((recipe => (
+                        <Recipe key={recipe._id}>
+                            <RecipeNavLink to={recipe._id}>
+                            <RecipeContainer>
+                                <Description>
+                                <p>{recipe.title}</p>
+                                </Description>
+                                <RecipeImg src={recipe.preview ? recipe.preview : IngredientsPlaceholder} loading="lazy" alt={recipe.title} />
+                            </RecipeContainer>
+                            </RecipeNavLink>
+                        </Recipe>
+                        )))}
+                    </List>
                     <button to={`/categories/${category}`}>See all</button>
                 </ListItem>
             ))}
-        </ul>
+        </List>
     )
 }
 
