65bde36d58574eaaf919de04a9f338597c18efb2
Khavkin <hdm@ukr.net>
2023-07-14 18:56:34 +0300

    comment unused code for git build

--- src/pages/MyRecipes.jsx
+++ src/pages/MyRecipes.jsx
@@ -1,12 +1,12 @@
 import MainTitle from 'components/MainTitle/MainTitle';
-import useOwnRecipes from 'hooks/useOwnRecipes';
+//import useOwnRecipes from 'hooks/useOwnRecipes';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getOwnRecipesThunk } from 'redux/Recipes/own/operations';
 
 const MyRecipes = () => {
   const dispatch = useDispatch();
-  const { status } = useOwnRecipes();
+  // const { status } = useOwnRecipes();
 
   useEffect(() => {
     dispatch(getOwnRecipesThunk({}));
