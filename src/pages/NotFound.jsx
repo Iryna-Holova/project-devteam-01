fc134df69a0fcc37f708a261118e1ac680699bd0
Iryna Holova <gis.golova@gmail.com>
2023-07-16 18:28:17 +0300

    not founf page bugfix

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,20 @@
-import Error from "components/notFound/NotFound";
+import { Icon, Text, Title } from 'components/NotFoundPage/NotFound.styled';
+import sprite from '../assets/images.svg';
+import MainTitle from 'components/MainTitle/MainTitle';
 
 const NotFound = () => {
-  return <Error />;
+  return (
+    <>
+      <MainTitle>Not Found</MainTitle>
+      <div style={{ textAlign: 'center' }}>
+        <Icon>
+          <use href={sprite + '#not-found-page'}></use>
+        </Icon>
+        <Title>We are sorry,</Title>
+        <Text>but the page you were looking for can’t be found..</Text>
+      </div>
+    </>
+  );
 };
 
 export default NotFound;
