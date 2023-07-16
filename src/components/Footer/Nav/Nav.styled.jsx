d1483a010f54a038ff56afdf585ea8b9ffc24db5
Dmytro Nahornyi <d3049999@gmail.com>
2023-07-16 12:09:55 +0300

    test update header
    Поки що додав те що є. Ще треба багато зробити і переробити. Збираю все до купи. Як відправлю останню версію, напишу.

--- src/components/Footer/Nav/Nav.styled.jsx
+++ src/components/Footer/Nav/Nav.styled.jsx
@@ -18,48 +18,17 @@ export const NavList = styled.ul`
   display: flex;
   flex-direction: column;
   align-items: center;
-  /* line-height: 18px; */
+
   gap: 18px;
-  /* margin-top: 64px; */
   list-style: none;
-  /* width: 151px; */
+
 
   @media (min-width: 768px) {
     align-items: flex-start;
-    /* margin-top: 50px;
-    margin-right: 94px; */
-    
   }
-  /* 
-  @media (max-width: 620px) {
-    margin-top: 50px;
-    margin-right: 40px;
-  }
-
-  @media (max-width: 563px) {
-    margin-top: 50px;
-    margin-right: 10px;
-  }
-
-  @media (max-width: 482px) {
-margin: 0;
-  } */
 `;
 
 export const NavItem = styled.li`
-  /* /* margin-right: 1rem;
-  margin-bottom: 24px;
-  font-size: 14px;
-  line-height: 18px;
-  
-  &:last-child {
-    margin-right: 0;
-  }
-
-  @media (max-width: 620px) {
-    margin-bottom: 14px;
-    margin-right: 0;
-  } */
 `;
 
 export const NavLinkStyled = styled(NavLink)`
