032f29f7b5370f9da8e535e717bb5d27655364fb
oreshnyk <55344626+oreshnyk@users.noreply.github.com>
2023-07-18 19:10:05 +0200

    async-select

--- src/components/AddRecipePage/AddRecipeForm/RecipeIngredients/RecipeIngredients.jsx
+++ src/components/AddRecipePage/AddRecipeForm/RecipeIngredients/RecipeIngredients.jsx
@@ -2,81 +2,82 @@ import React, { useEffect } from 'react';
 import { FieldArray, useFormikContext } from 'formik';
 import { useDispatch, useSelector } from 'react-redux';
 import { getIngredientsThunk } from '../../../../redux/Ingredients/operations';
-import { selectIngredients } from '../../../../redux/Ingredients/selectors'
+import { selectIngredients } from '../../../../redux/Ingredients/selectors';
+import AsyncSelect from 'react-select/async';
 
 const RecipeIngredients = () => {
-    const { values, setFieldValue } = useFormikContext();
+  const { values, setFieldValue } = useFormikContext();
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getIngredientsThunk());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(getIngredientsThunk());
+  }, [dispatch]);
 
-    const ingredients = useSelector(selectIngredients);
+  const ingredients = useSelector(selectIngredients);
 
-    // + - buttons
-    // const handleRemoveIngredient = (index) => {
-    // };
-
-    // const handleAddIngredient = () => {
-    // };
-    //
-
-    return (
-        <div>
-            <h2>Ingredients</h2>
-            {/* <button type="button" onClick={() => handleRemoveIngredient(index)}>
-                -
-            </button> */}
-            <p>{values.ingredients.length}</p>
-            {/* <button type="button" onClick={() => handleAddIngredient()}>
-                +
-            </button> */}
-            <FieldArray
-                name="ingredients"
-                render={({ push, remove }) => (
-                    <>
-                        {values.ingredients.map((ingredient, index) => (
-                            <div key={index}>
-                                <label htmlFor={`ingredients[${index}].name`}>Ingredient Name:</label>
-                                <select
-                                    id={`ingredients[${index}].name`}
-                                    name={`ingredients[${index}].name`}
-                                    value={ingredient.name}
-                                    onChange={(e) => setFieldValue(`ingredients[${index}].name`, e.target.value)}
-                                >
-                                    {/* зробити dropdown? */}
-                                    {ingredients.map((option) => (
-                                        <option key={option._id.$oid} value={option._id.$oid}>
-                                            {option.name}
-                                        </option>
-                                    ))}
-                                </select>
-
-                                <label htmlFor={`ingredients[${index}].measure`}>Measure:</label>
-                                <input
-                                    type="text"
-                                    id={`ingredients[${index}].measure`}
-                                    name={`ingredients[${index}].measure`}
-                                    value={ingredient.measure}
-                                    onChange={(e) => setFieldValue(`ingredients[${index}].measure`, e.target.value)}
-                                />
-
-                                <button type="button" onClick={() => remove(index)}>
-                                    X
-                                </button>
-                            </div>
-                        ))}
-
-                        <button type="button" onClick={() => push({ name: '', measure: '' })}>
-                            Add Ingredient
-                        </button>
-                    </>
-                )}
-            />
-        </div>
+  const filterIngredients = (inputValue) => {
+    return ingredients.filter((ingredient) =>
+      ingredient.name.toLowerCase().includes(inputValue.toLowerCase())
     );
+  };
+
+  const loadIngredients = (inputValue, callback) => {
+    setTimeout(() => {
+      const filteredIngredients = filterIngredients(inputValue);
+      callback(filteredIngredients);
+    }, 1000);
+  };
+
+  const handleIngredientChange = (index, selectedOption) => {
+    setFieldValue(`ingredients[${index}].name`, selectedOption ? selectedOption : null);
+  };
+
+  return (
+    <div>
+      <h2>Ingredients</h2>
+      <FieldArray
+        name="ingredients"
+        render={({ push, remove }) => (
+          <>
+            {values.ingredients.map((ingredient, index) => (
+              <div key={index}>
+                <label htmlFor={`ingredients[${index}].name`}>Ingredient Name:</label>
+                <AsyncSelect
+                  id={`ingredients[${index}].name`}
+                  name={`ingredients[${index}].name`}
+                  cacheOptions
+                  defaultOptions
+                  loadOptions={(inputValue, callback) => loadIngredients(inputValue, callback)}
+                  getOptionLabel={(option) => option.name}
+                  getOptionValue={(option) => option}
+                  value={ingredient.name}
+                  onChange={(selectedOption) => handleIngredientChange(index, selectedOption)}
+                />
+
+                <label htmlFor={`ingredients[${index}].measure`}>Measure:</label>
+                <input
+                  type="text"
+                  id={`ingredients[${index}].measure`}
+                  name={`ingredients[${index}].measure`}
+                  value={ingredient.measure}
+                  onChange={(e) => setFieldValue(`ingredients[${index}].measure`, e.target.value)}
+                />
+
+                <button type="button" onClick={() => remove(index)}>
+                  X
+                </button>
+              </div>
+            ))}
+
+            <button type="button" onClick={() => push({ name: '', measure: '' })}>
+              Add Ingredient
+            </button>
+          </>
+        )}
+      />
+    </div>
+  );
 };
 
 export default RecipeIngredients;
