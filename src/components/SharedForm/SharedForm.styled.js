1886eacd66c89e2553c412ce73786fa12cf128ba
Iryna Holova <gis.golova@gmail.com>
2023-07-18 19:58:37 +0300

    fixed validation messages && add bg

--- src/components/SharedForm/SharedForm.styled.js
+++ src/components/SharedForm/SharedForm.styled.js
@@ -1,62 +1,131 @@
 import { styled } from 'styled-components';
-
+import { Link } from 'react-router-dom';
 import { Form, Field } from 'formik';
+import bgMobile from '../../assets/svg/authFormOrder/bg-registration-mobile.svg';
+import bgTablet from '../../assets/svg/authFormOrder/bg-registration-tablet.svg';
+import bgDesktop from '../../assets/svg/authFormOrder/bg-registration-desktop.svg';
+
+export const Wrapper = styled.div`
+  box-sizing: border-box;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column-reverse;
+  align-items: center;
+  padding-top: 87px;
+  background-image: url(${bgMobile});
+  background-size: 100% 490px;
+  background-repeat: no-repeat;
+  background-position: top 322px center;
+
+  @media screen and (min-width: 768px) {
+    padding-top: 96px;
+    background-image: url(${bgTablet});
+    background-size: 100% 606px;
+    background-position: top 419px center;
+  }
+
+  @media screen and (min-width: 1440px) {
+    flex-direction: row-reverse;
+    gap: 115px;
+    justify-content: center;
+    padding: 0;
+    background-image: url(${bgDesktop});
+    background-size: 100% 325px;
+    background-position: bottom center;
+  }
+`;
+
+export const Image = styled.svg`
+  width: 285px;
+  height: 250px;
+
+  @media screen and (min-width: 768px) {
+    width: 409px;
+    height: 359px;
+  }
+  @media screen and (min-width: 1440px) {
+    width: 532px;
+    height: 468px;
+  }
+`;
 
 export const StyledForm = styled(Form)`
-  flex-shrink: 0;
+  box-sizing: border-box;
+  margin-top: -32px;
+  width: 335px;
+  padding: 32px 28px;
   border-radius: 30px;
-  background: #2a2c36;
-  box-shadow: 0px 4px 48px 0px rgba(0, 0, 0, 0.1);
-  display: flex;
-  flex-direction: column;
-  padding: 32px 28px 40px;
+  background-color: var(--color-start-bg);
 
-
-  max-width: 335px;
-  max-height: 350px;
-
-  @media screen and (min-width: 768px){
-    max-width: 500px;
-    max-height: 384px;
-  padding: 44px 50px;
+  @media screen and (min-width: 768px) {
+    margin-top: -11px;
+    width: 500px;
+    padding: 50px;
   }
-  
-  `;
+`;
 
 export const WordForm = styled.h2`
-  color: #fafafa;
-  font-size: 28px;
+  text-align: left;
+  color: var(--color-start-text);
+  font-size: 24px;
   font-weight: 600;
-  line-height: 30px;
-  letter-spacing: -0.56px;
-  margin-bottom: 32px;
+  line-height: 28px;
+  letter-spacing: -0.48px;
+  margin-bottom: 18px;
+  @media screen and (min-width: 768px) {
+    font-size: 28px;
+    font-weight: 600;
+    line-height: 30px;
+    letter-spacing: -0.56px;
+    margin-bottom: 32px;
+  }
+`;
+
+export const List = styled.ul`
+  gap: 24px;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 28px;
+  list-style: none;
+
+  @media screen and (min-width: 768px) {
+    margin-bottom: 50px;
+  }
+`;
+
+export const FieldWrapper = styled.div`
+  position: relative;
+
+  & svg {
+    position: absolute;
+    width: 18px;
+    height: 18px;
+    left: 14px;
+    top: 14px;
+
+    @media screen and (min-width: 768px) {
+      width: 24px;
+      height: 24px;
+      left: 18px;
+      top: 18px;
+    }
+  }
 `;
 
 export const StyledField = styled(Field)`
-  width: 279px;
-  flex-shrink: 0;
+  box-sizing: border-box;
+  color: var(--color-start-text);
+  width: 100%;
   stroke-width: 1px;
   stroke: #fff;
-  padding-top: 16px;
-  padding-bottom: 16px;
-  padding-left: 50px;
+  padding: 12px 12px 12px 40px;
   background: transparent;
   border-radius: 6px;
   border: 1px solid gray;
-  color: #fafafa;
   outline: none;
-
-  &::placeholder {
-    color: #ffffff;
-
-    font-family: Poppins;
-    font-size: 18px;
-    letter-spacing: -0.36px;
-  }
-
-  &:focus::placeholder {
-    color: white;
-  }
+  font-family: Poppins;
+  font-size: 14px;
+  letter-spacing: -0.36px;
 
   &:focus,
   &:hover {
@@ -64,58 +133,47 @@ export const StyledField = styled(Field)`
   }
 
   @media screen and (min-width: 768px) {
-    width: 340px;
+    padding: 16px 16px 16px 50px;
+    font-size: 18px;
   }
-
-
-`;
-
-export const List = styled.ul`
-  gap: 24px;
-  display: flex;
-  flex-direction: column;
-  list-style: none;
 `;
 
 export const SubButton = styled.button`
-  padding: 12px 106px;
-  margin-top: 50px;
-  cursor: pointer;
+  padding: 12px;
   border-radius: 6px;
-  background: #8baa36;
-  color: #fafafa;
-  font-family: Poppins;
+  width: 100%;
+  background: var(--color-accent);
+  color: var(--color-start-text);
   font-size: 16px;
-  border: none;
   line-height: 18px;
+  transition: color var(--transition-time) var(--cubic);
 
-  @media screen and (min-width: 768px){
-    padding: 21px 138px;
-    
+  &:hover {
+    color: var(--color-icons);
   }
-
+  @media screen and (min-width: 768px) {
+    padding: 21px;
+  }
 `;
 
-export const Router = styled.p`
-  color: black;
+export const Router = styled(Link)`
+  line-height: 57px;
   margin-top: 18px;
+  text-decoration: underline;
+  color: var(--color-start-text);
+  transition: color var(--transition-time) var(--cubic);
+
+  &:hover {
+    color: var(--color-accent);
+  }
 `;
 
-export const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+export const Error = styled.div`
+  font-size: 12px;
+  color: #e74a3b;
   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  bottom: -16px;
+  & ~ input {
+    border-color: #e74a3b;
+  }
 `;
-
-// @media screen and (min-width: 768px) {
-
-// }
-
-// @media screen and (min-width: 1440px) {
-
-// }
