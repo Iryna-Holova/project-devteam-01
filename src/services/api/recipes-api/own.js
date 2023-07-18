c7ca1b9ac7f6e3ef1ea670668a4e8df5d016ccf0
Khavkin <hdm@ukr.net>
2023-07-18 20:42:53 +0300

    changed endpoints according to backend

--- src/services/api/recipes-api/own.js
+++ src/services/api/recipes-api/own.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
-import { BASE_URL} from './common';
+import { BASE_URL } from './common';
 
 export const getOwnRecipes = async ({ page = 1, limit = 4 }) => {
   axios.defaults.baseURL = BASE_URL;
   try {
- //   setAuthHeader(axios, token);
+    //   setAuthHeader(axios, token);
 
     const { status, data } = await axios.get(
-      `/api/own-recipes/?page=${page}&limit=${limit}`
+      `/api/recipes/own/?page=${page}&limit=${limit}`
     );
-   // clearAuthHeader(axios);
+    // clearAuthHeader(axios);
     if (status === 200) {
       //  console.log(data);
       return data;
