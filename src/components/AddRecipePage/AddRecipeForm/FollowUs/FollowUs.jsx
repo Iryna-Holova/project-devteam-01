6618356652a26d19f56ac3302e8826eae8a9b759
Iryna Holova <gis.golova@gmail.com>
2023-07-19 20:01:16 +0300

    bugfix

--- src/components/AddRecipePage/AddRecipeForm/FollowUs/FollowUs.jsx
+++ src/components/AddRecipePage/AddRecipeForm/FollowUs/FollowUs.jsx
@@ -1,7 +1,7 @@
-import {ReactComponent as Facebook} from '../../../../assets/svg/FollowUsIcons/facebook.svg';
-import { ReactComponent as Youtube } from '../../../../assets/svg/FollowUsIcons/youTube.svg';
-import { ReactComponent as Twitter }from '../../../../assets/svg/FollowUsIcons/twiter.svg';
-import { ReactComponent as Instagram } from '../../../../assets/svg/FollowUsIcons/instagram.svg';
+// import {ReactComponent as Facebook} from '../../../../assets/svg/FollowUsIcons/facebook.svg';
+// import { ReactComponent as Youtube } from '../../../../assets/svg/FollowUsIcons/youTube.svg';
+// import { ReactComponent as Twitter }from '../../../../assets/svg/FollowUsIcons/twiter.svg';
+// import { ReactComponent as Instagram } from '../../../../assets/svg/FollowUsIcons/instagram.svg';
 import { StyledFollowUsBlock, SocialMediaList, Link } from "./FollowUs.styled";
 import { SubTitleStyled } from '../RecipePreparation/RecipePreparation.styled';
 const FollowUs = () => {
@@ -11,21 +11,21 @@ const FollowUs = () => {
         <div>
             <SocialMediaList>
                          <Link href="https://www.facebook.com/" >
-                        <Facebook width="26" height="26"/>
+                        {/* <Facebook width="26" height="26"/> */}
                     </Link>
 
                
                     <Link href="https://www.youtube.com/">
-                            <Youtube width="28" height="22"/>
+                            {/* <Youtube width="28" height="22"/> */}
                     </Link>
               
                     <Link href="https://www.twitter.com/">
-                            <Twitter width="25" height="22"/>
+                            {/* <Twitter width="25" height="22"/> */}
                     </Link>
                
                 
                     <Link href="https://www.instagram.com/">
-                            <Instagram width="24" height="24"/>
+                            {/* <Instagram width="24" height="24"/> */}
                     </Link>
               
             </SocialMediaList>
