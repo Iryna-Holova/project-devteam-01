605b67452b02c6a42784f1884978fcd0b1b29d1c
Iryna Holova <gis.golova@gmail.com>
2023-07-19 08:27:45 +0300

    add my-recipes page

--- src/pages/MyRecipes.jsx
+++ src/pages/MyRecipes.jsx
@@ -1,18 +1,49 @@
-import MainTitle from 'components/MainTitle/MainTitle';
 //import useOwnRecipes from 'hooks/useOwnRecipes';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getOwnRecipesThunk } from 'redux/Recipes/own/operations';
+import useOwnRecipes from 'hooks/useOwnRecipes';
+
+import MainTitle from 'components/MainTitle/MainTitle';
+import { Loader } from 'components/loader/loader';
+import NoDataMessage from 'components/NoDataMessage/NoDataMessage';
+import { RecipesList } from 'components/RecipesList/RecipesList';
 
 const MyRecipes = () => {
+  const { isLoading, data, page, total, pages, limit } = useOwnRecipes();
+
   const dispatch = useDispatch();
-  // const { status } = useOwnRecipes();
 
   useEffect(() => {
-    dispatch(getOwnRecipesThunk({}));
-  }, [dispatch]);
+    dispatch(getOwnRecipesThunk({ page, limit }));
+  }, [dispatch, page, limit, total, pages,]);
 
-  return <MainTitle>My Recipes page</MainTitle>;
+  const handleRemoveClick = recipeId => {
+    // TODO
+  };
+
+  return (
+    <>
+      <MainTitle>My recipes</MainTitle>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          {!data.length ? (
+            <NoDataMessage>
+              You don't have any added recipes yet...
+            </NoDataMessage>
+          ) : (
+            <RecipesList
+              data={data}
+              removeRecipe={handleRemoveClick}
+              className="own-recipes"
+            />
+          )}
+        </>
+      )}
+    </>
+  );
 };
 
 export default MyRecipes;
