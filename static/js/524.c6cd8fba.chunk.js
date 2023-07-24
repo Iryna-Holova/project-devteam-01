cdbf86fb1c316e749bf9c2c1f8afcb8f0108824e
Iryna-Holova <94083644+Iryna-Holova@users.noreply.github.com>
2023-07-24 07:21:28 +0000

    Deploying to gh-pages from @ Iryna-Holova/project-devteam-01@25e91d1b167bae88cac1d972d3292bff03013a82 🚀

--- static/js/524.c6cd8fba.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[524],{3524:function(t,e,a){a.d(e,{Lff:function(){return n}});var r=a(9983);function n(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 256 256",fill:"currentColor"},child:[{tag:"path",attr:{d:"M208,56H180.28L166.65,35.56A8,8,0,0,0,160,32H96a8,8,0,0,0-6.65,3.56L75.71,56H48A24,24,0,0,0,24,80V192a24,24,0,0,0,24,24H208a24,24,0,0,0,24-24V80A24,24,0,0,0,208,56Zm-44,76a36,36,0,1,1-36-36A36,36,0,0,1,164,132Z"}}]})(t)}}}]);
-//# sourceMappingURL=524.c6cd8fba.chunk.js.map
