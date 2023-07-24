1bf4d67d0b8cd33ecb5e410503af2bca7c77f918
Khavkin <hdm@ukr.net>
2023-07-24 21:40:36 +0300

    FIX erros in Redux and apis `

--- src/services/api/recipes-api/favorite.js
+++ src/services/api/recipes-api/favorite.js
@@ -4,23 +4,14 @@ import { BASE_URL } from './common';
 export const getFavorite = async ({ limit = 99999, page = 1 }) => {
   axios.defaults.baseURL = BASE_URL;
   try {
-    //console.log(axios.defaults);
-    // setAuthHeader(
-    //   axios,
-    //   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YWRjZTkwMjc0ZGFmYWIyN2JlOTkyZSIsImlhdCI6MTY4OTQzMzU1MiwiZXhwIjoxNjg5NTE2MzUyfQ.4cKZG1aHnDuVwc6WPn59Yyj-chgLcK5OkGhhN8dLVXo'
-    // );
-
     const { status, data } = await axios.get(
       `/api/recipes/favorites/?page=${page}&limit=${limit}`
     );
-    // clearAuthHeader(axios);
 
     if (status === 200) {
-      // console.log(data);
       return data;
     } else return null;
   } catch (error) {
-    // console.log(error);
     return Promise.reject(new Error(error.response.data.message));
   }
 };
@@ -28,11 +19,6 @@ export const getFavorite = async ({ limit = 99999, page = 1 }) => {
 export const addToFavorite = async ({ id }) => {
   axios.defaults.baseURL = BASE_URL;
   try {
-    // setAuthHeader(
-    //   axios,
-    //   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YWRjZTkwMjc0ZGFmYWIyN2JlOTkyZSIsImlhdCI6MTY4OTQzMzU1MiwiZXhwIjoxNjg5NTE2MzUyfQ.4cKZG1aHnDuVwc6WPn59Yyj-chgLcK5OkGhhN8dLVXo'
-    // );
-
     const { status, data } = await axios.patch(
       `/api/recipes/favorites/`,
       { recipeId: id },
@@ -42,13 +28,7 @@ export const addToFavorite = async ({ id }) => {
         },
       }
     );
-    // clearAuthHeader(axios);
     return { status, message: data.message };
-    //if (status === 200) {
-
-    //     console.log(data);
-    //     return data;
-    //   } else return null;
   } catch (error) {
     // console.log(error);
     return Promise.reject(new Error(error.response.data.message));
@@ -58,19 +38,8 @@ export const addToFavorite = async ({ id }) => {
 export const deleteFromFavorite = async ({ id }) => {
   axios.defaults.baseURL = BASE_URL;
   try {
-    // setAuthHeader(
-    //   axios,
-    //   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YWRjZTkwMjc0ZGFmYWIyN2JlOTkyZSIsImlhdCI6MTY4OTQzMzU1MiwiZXhwIjoxNjg5NTE2MzUyfQ.4cKZG1aHnDuVwc6WPn59Yyj-chgLcK5OkGhhN8dLVXo'
-    // );
-
     const { status, data } = await axios.delete(`/api/recipes/favorites/${id}`);
-    // clearAuthHeader(axios);
-    // console.log(data);
     return { status, message: data.message };
-    // if (status === 200) {
-    //   console.log(data);
-    //   return data;
-    // } else return null;
   } catch (error) {
     //  console.log(error);
     return Promise.reject(new Error(error.response.data.message));
