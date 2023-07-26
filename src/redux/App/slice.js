1c8ad4e0fe3c9eaf98b39ef1df436363e34403e5
Iryna Holova <gis.golova@gmail.com>
2023-07-26 18:38:20 +0300

    Update slice.js

--- src/redux/App/slice.js
+++ src/redux/App/slice.js
@@ -12,7 +12,6 @@ export const appSlice = createSlice({
   initialState,
   reducers: {
     setTheme(state, { payload }) {
-      console.log(payload);
       state.theme = payload;
     },
     setDevice(state, { payload }) {
